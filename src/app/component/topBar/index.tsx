f03674e157611e048def284532d28ee3a37992f0
tinymonster123 <lfhxgs123@gmail.com>
2025-02-26 16:15:36 +0800

    fix: 停止在 topbar 中使用动画组件

--- src/app/component/topBar/index.tsx
+++ src/app/component/topBar/index.tsx
@@ -1,38 +1,46 @@
 "use client";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import TextFade from "../textfade";
+// import TextFade from "../textfade";
 
 const TopBar = () => {
+  const [isStart, setStart] = useState(false);
+
+  useEffect(() => {
+    setStart(true);
+  }, []);
+
   return (
-    <TextFade direction="down" delay={5} >
-      <header
-        className={`fixed top-0 left-0 right-0 bg-white backdrop-blur-sm z-50 `}
-      >
-        <nav className="max-w-7xl mx-auto px-4 flex items-center justify-between h-14 md:h-16">
+    // <TextFade direction="down" delay={5} >
+    <header
+      className={`fixed top-0 left-0 right-0 bg-white backdrop-blur-sm z-50 transition-transform ease-in-out duration-3000 delay-3000 ${
+        isStart ? "transition-y-0" : "-transition-y-full"
+      }`}
+    >
+      <nav className="max-w-7xl mx-auto px-4 flex items-center justify-between h-14 md:h-16">
+        <Link
+          href="https://github.com/tinymonster123"
+          className="font-medium hover:text-pinky-deepRed transition-colors text-black"
+        >
+          About Me
+        </Link>
+        <div className="flex items-center gap-6">
           <Link
-            href="https://github.com/tinymonster123"
-            className="font-medium hover:text-pinky-deepRed transition-colors text-black"
+            href="/signup"
+            className="hover:text-pinky-deepRed transition-colors text-black"
           >
-            About Me
+            Sign Up
           </Link>
-          <div className="flex items-center gap-6">
-            <Link
-              href="/signup"
-              className="hover:text-pinky-deepRed transition-colors text-black"
-            >
-              Sign Up
-            </Link>
-            <Link
-              href="/pages/login"
-              className="px-6 py-2 h-12 bg-pinky-deepRed text-black rounded-lg hover:bg-pinky-lightRed transition-colors "
-            >
-              Log In
-            </Link>
-          </div>
-        </nav>
-      </header>
-    </TextFade>
+          <Link
+            href="/pages/login"
+            className="px-6 py-2 h-12 bg-pinky-deepRed text-black rounded-lg hover:bg-pinky-lightRed transition-colors "
+          >
+            Log In
+          </Link>
+        </div>
+      </nav>
+    </header>
+    // </TextFade>
   );
 };
 
