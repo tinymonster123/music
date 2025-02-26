0015210183649b5e1584122e637048d0341a0afc
tinymonster123 <lfhxgs123@gmail.com>
2025-02-26 16:16:02 +0800

    pref: 修改定位路径

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -14,11 +14,11 @@ const LoginPage = () => {
   
 
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh ">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-primary text-primary-foreground">
+        <div className="flex justify-start gap-2 ">
+          <a href="/" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-start justify-center rounded-full bg-primary text-primary-foreground">
               <ArrowLeft />
             </div>
           </a>
@@ -29,13 +29,13 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
+      {/* <div className="relative hidden bg-muted lg:block">
         <img
           src="/placeholder.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
-      </div>
+      </div> */}
     </div>
   );
 };
