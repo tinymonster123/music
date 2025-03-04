56d000205f3ad681d7e057ed1007ceab822c179e
tinymonster123 <lfhxgs123@gmail.com>
2025-03-05 00:10:25 +0800

    feat: 添加 homepage 和 datapage 导航功能

--- src/app/component/topBar/index.tsx
+++ src/app/component/topBar/index.tsx
@@ -1,9 +1,12 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const TopBar = () => {
   const [isStart, setStart] = useState(false);
+  const path = usePathname();
+  const isHome = path === "/";
 
   useEffect(() => {
     setStart(true);
@@ -12,17 +15,34 @@ const TopBar = () => {
   return (
     // <TextFade direction="down" delay={5} >
     <header
-      className={`fixed top-0 left-0 right-0 bg-white backdrop-blur-sm z-50 transition-transform ease-in-out duration-3000 delay-3000 ${
+      className={` top-0 left-0 right-0 bg-white backdrop-blur-sm z-50 transition-transform ease-in-out duration-3000 delay-3000 ${
         isStart ? "transition-y-0" : "-transition-y-full"
       }`}
     >
       <nav className="max-w-7xl mx-auto px-4 flex items-center justify-between h-14 md:h-16">
-        <Link
-          href="https://github.com/tinymonster123"
-          className="font-medium hover:text-pinky-deepRed transition-colors text-black"
-        >
-          About Me
-        </Link>
+        <div className="flex items-center gap-6">
+          <Link
+            href="https://github.com/tinymonster123"
+            className="font-medium hover:text-pinky-deepRed transition-colors text-black"
+          >
+            About Me
+          </Link>
+          {!isHome ? (
+            <Link
+              href="/"
+              className="font-medium hover:text-pinky-deepRed transition-colors text-black"
+            >
+              HomePage
+            </Link>
+          ) : (
+            <Link
+              href="/pages/showpage"
+              className="font-medium hover:text-pinky-deepRed transition-colors text-black"
+            >
+              Data Page
+            </Link>
+          )}
+        </div>
         <div className="flex items-center gap-6">
           <Link
             href="/pages/signup"
