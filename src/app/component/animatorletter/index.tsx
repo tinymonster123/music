77250616d5466d0d5c1570a3e6aa3e150abec504
tinymonster123 <lfhxgs123@gmail.com>
2025-03-05 08:44:19 +0800

    feat: 限制首屏动画再次打开时加载

--- src/app/component/animatorletter/index.tsx
+++ src/app/component/animatorletter/index.tsx
@@ -1,16 +1,32 @@
 "use client";
+import { useEffect } from "react";
 import Animator from "../../animation";
 import SVGPath from "@/app/animation/path";
 
-const AnimatorLetter = () => {
+interface AnimationComplete {
+  handleAnimationComplete: () => void;
+}
+
+const AnimatorLetter = ({ handleAnimationComplete }: AnimationComplete) => {
   // console.log(SVGPath);
+  const animationDuration = 5000;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (handleAnimationComplete) {
+        handleAnimationComplete();
+      }
+    }, animationDuration);
+
+    return () => clearTimeout(timer);
+  }, [handleAnimationComplete]);
 
   return (
     <div className="w-full h-screen flex items-center justify-center bg-white transform-gpu">
       <Animator
         pathAnimation={{
           type: "tween",
-          duration: 5, // 鎸佺画鏃堕棿鍙互鏍规嵁闇€瑕佽皟鏁�
+          duration: 5, // 閹镐胶鐢婚弮鍫曟？閸欘垯浜掗弽瑙勫祦闂団偓鐟曚浇鐨熼弫锟�
           ease: "easeInOut",
         }}
         from={0}
--- src/app/pages/homepage/index.tsx
+++ src/app/pages/homepage/index.tsx
@@ -1,13 +1,44 @@
-﻿import TopBar from "@/app/component/topBar";
+"use client";
+import TopBar from "@/app/component/topBar";
 import AnimatorLetter from "@/app/component/animatorletter";
+import Letter from "../../../assets/images/letter.svg";
 import LazyLoader from "@/app/component/lazyloader";
 import Introduction from "../introduction";
+import { useEffect, useState } from "react";
+
+const StaticLetter = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-white transform-gpu">
+      <Letter />
+    </div>
+  );
+};
 
 const HomePage = () => {
+  const [showAnimation, setShowAnimation] = useState(false);
+  const key = "hasBeenWatched";
+
+  const hasBeenWatchedAnimation = sessionStorage.getItem(key);
+
+  useEffect(() => {
+    if (!hasBeenWatchedAnimation) {
+      setShowAnimation(true);
+      sessionStorage.setItem(key, "true");
+    }
+  }, []);
+
+  const handleAnimationComplete = () => {
+    setShowAnimation(false);
+  };
+
   return (
     <>
       <TopBar />
-      <AnimatorLetter />
+      {showAnimation ? (
+        <AnimatorLetter handleAnimationComplete={handleAnimationComplete} />
+      ) : (
+        <StaticLetter />
+      )}
       <LazyLoader>
         <Introduction />
       </LazyLoader>
