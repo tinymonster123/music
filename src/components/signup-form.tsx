698fef64bb12733f926bb47e9f4396b9b21ba86b
tinymonster123 <lfhxgs123@gmail.com>
2025-03-07 11:54:13 +0800

    feat: 添加 github oauth 登录功能

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -6,7 +6,10 @@ import { Label } from "@/components/ui/label";
 import { CustomRef } from "@/app/hooks/emailandpassword";
 import useEmailAndPasswordStore from "@/app/hooks/emailandpassword";
 import { useEffect, useRef, useState } from "react";
-// import { debounce } from "framer/render/utils/debounce.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const authorizeURL = `${process.env.NEXT_PUBLIC_AUTHORIZE_URL}?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&scope=user:email&state=melomane&prompt=consent`;
 
 const SignUpForm: React.FC<CustomRef> = ({
   className,
@@ -17,6 +20,7 @@ const SignUpForm: React.FC<CustomRef> = ({
   const { setEmailRef, setPasswordRef } = useEmailAndPasswordStore();
   const confirmRef = useRef<HTMLInputElement>(null);
   const [error, setError] = useState<boolean>(true);
+  const [ifRequired, setRequired] = useState<boolean>(true);
 
   useEffect(() => {
     if (emailRef) setEmailRef(emailRef);
@@ -31,6 +35,13 @@ const SignUpForm: React.FC<CustomRef> = ({
     }
 
     setError(true);
+  };
+
+  const handleGithubClick = () => {
+    setRequired(false);
+    if (authorizeURL) {
+      window.location.href = authorizeURL;
+    }
   };
 
   return (
@@ -48,29 +59,50 @@ const SignUpForm: React.FC<CustomRef> = ({
       <div className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="m@example.com"
-            required
-            ref={emailRef}
-          />
+          {ifRequired ? (
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+              ref={emailRef}
+            />
+          ) : (
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              ref={emailRef}
+            />
+          )}
         </div>
         <div className="grid gap-2">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
           </div>
-          <Input id="password" type="password" required ref={passwordRef} />
+          {ifRequired ? (
+            <Input id="password" type="password" required ref={passwordRef} />
+          ) : (
+            <Input id="password" type="password" ref={passwordRef} />
+          )}
         </div>
         <div>
           <div className="grid gap-2">
             <div className="flex items-center">
               <Label htmlFor="password">Confirm Password</Label>
             </div>
-            <Input id="password" type="password" required ref={confirmRef} />
+            {ifRequired ? (
+              <Input id="password" type="password" required ref={confirmRef} />
+            ) : (
+              <Input id="password" type="password" ref={confirmRef} />
+            )}
           </div>
         </div>
-        <Button type="submit" className="w-full">
+        <Button
+          type="submit"
+          className="w-full"
+          onClick={() => setRequired(true)}
+        >
           Sign Up
         </Button>
         <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
@@ -78,7 +110,11 @@ const SignUpForm: React.FC<CustomRef> = ({
             Or continue with
           </span>
         </div>
-        <Button variant="outline" className="w-full">
+        <Button
+          variant="outline"
+          className="w-full"
+          onClick={handleGithubClick}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path
               d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
