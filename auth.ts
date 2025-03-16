b4166b3991c1a2ff8b89bc5e646a3949cde3633b
tinymonster123 <lfhxgs123@gmail.com>
2025-03-17 00:45:31 +0800

    update: 更新 authjs 以使用 prisma adapter

--- auth.ts
+++ auth.ts
@@ -1,5 +1,24 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+import GitHub from "next-auth/providers/github";
+import type { NextAuthConfig } from "next-auth";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-});
+export const config = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+  basePath: "/auth",
+  callbacks: {
+    jwt({ token, trigger, session }) {
+      if (trigger === "update") token.name = session.user.name;
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
+
+export const { handlers, signIn, signOut, auth } = NextAuth(config);
