b761eb1fe1f1803b6d011035e9eb8a6cdde4e459
tinymonster123 <lfhxgs123@gmail.com>
2025-03-18 00:41:12 +0800

    feat: 修改数据库连接逻辑

--- auth.ts
+++ auth.ts
@@ -1,29 +1,12 @@
 import NextAuth from "next-auth";
 import PostgresAdapter from "@auth/pg-adapter";
-import { Pool } from "pg";
-import { getPgPool } from "@/app/api/connection/connectpsql";
 import GitHub from "next-auth/providers/github";
-import type { NextAuthConfig } from "next-auth";
+import { Pool } from "pg";
 
-let pgPromise: any;
-
-pgPromise = getPgPool(pgPromise);
-
-export const config = {
-  adapter: PostgresAdapter(pgPromise),
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
-  ],
-  basePath: "/auth",
-  callbacks: {
-    jwt({ token, trigger, session }) {
-      if (trigger === "update") token.name = session.user.name;
-      return token;
-    },
-  },
-} satisfies NextAuthConfig;
-
-export const { handlers, signIn, signOut, auth } = NextAuth(config);
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  return {
+    adapter: PostgresAdapter(pool),
+    providers: [GitHub],
+  };
+});
