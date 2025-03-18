8404d80998417130baf04148927d1d1e12c64531
tinymonster123 <lfhxgs123@gmail.com>
2025-03-19 00:41:35 +0800

    style: 更新 topbar 样式

--- src/app/component/sessionTopBar/index.tsx
+++ src/app/component/sessionTopBar/index.tsx
@@ -54,7 +54,7 @@ const SessionTopBar = () => {
           </div>
 
           <button
-            className="px-4 py-2 rounded-md bg-gray-100 hover:bg-gray-200 text-gray-700 text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pinky-deepRed"
+            className="px-4 py-2 rounded-md bg-[#ff0000] text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pinky-deepRed"
             onClick={async () => {
               await signOut({ redirect: false });
               router.push("/");
