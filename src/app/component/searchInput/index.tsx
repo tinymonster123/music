ea04b45573a010c685a4179aa20e3416a4d46f4b
tinymonster123 <lfhxgs123@gmail.com>
2025-03-23 15:56:07 +0800

    feat: 触发请求 text2sql 接口功能

--- src/app/component/searchInput/index.tsx
+++ src/app/component/searchInput/index.tsx
@@ -1,13 +1,89 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+import { useSQLStore } from "@/app/hooks/sqldate";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 
 const SearchInput = () => {
+  const { status } = useSession();
+  const { toast } = useToast();
+  const [query, setQuery] = useState("");
+  const { setSQL } = useSQLStore();
+  const ifAuth = status === "authenticated";
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!ifAuth) {
+      toast({
+        description: "请先进行登录",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    if (!query.trim()) {
+      toast({
+        description: "搜索内容不能为空",
+        variant: "destructive",
+      });
+      return;
+    }
+    try {
+      const response = await axios.post("/api/text2sql", {
+        query: query.trim(),
+      });
+      const data = response.data;
+      if (data.status === 200 && data.success) {
+        setSQL(data.sql);
+        toast({
+          description: "成功实现 text to sql",
+        });
+      } else {
+        switch (data.status) {
+          case 400:
+            toast({
+              description: "未登录错误",
+              variant: "destructive",
+            });
+            break;
+          case 401:
+            toast({
+              description: "请求错误",
+              variant: "destructive",
+            });
+            break;
+          default:
+            toast({
+              description: "请求错误",
+              variant: "destructive",
+            });
+        }
+      }
+    } catch (error: any) {
+      console.error(error.response.data.message);
+      const errorMessage = error.response.data.message || "请求错误";
+      toast({
+        description: `${errorMessage}`,
+        variant: "destructive",
+      });
+      throw error;
+    }
+  };
+
   return (
-    <form className="h-20 flex justify-center items-center w-full px-4 md:px-8">
+    <form
+      className="h-20 flex justify-center items-center w-full px-4 md:px-8"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-row gap-2 md:gap-4 w-full max-w-2xl">
         <div className="relative flex-1">
           <Input
             placeholder="Search for music"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
             className="pr-10 h-11 rounded-lg border-gray-300 focus:ring-2 focus:ring-red-100 focus:border-none transition-all"
           />
           <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none text-gray-400">
