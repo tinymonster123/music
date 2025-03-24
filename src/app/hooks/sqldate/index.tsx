f26edf54b16441c16f3498c146d8777bd650f048
tinymonster123 <lfhxgs123@gmail.com>
2025-03-25 00:06:44 +0800

    fix: 修改存储数据结构

--- src/app/hooks/sqldate/index.tsx
+++ src/app/hooks/sqldate/index.tsx
@@ -2,14 +2,14 @@ import { create } from "zustand";
 
 export interface SQLdate {
   columns: string[] | null;
-  sqlColumns: string[] | null;
-  setSQL: (sqlColumns: string[] | null) => void;
+  sqlColumns: Object[] | null;
+  setSQL: (sqlColumns: Object[] | null) => void;
   setColumns: (columns: string[] | null) => void;
 }
 
 export const useSQLStore = create<SQLdate>((set) => ({
   columns: null,
   sqlColumns: null,
-  setSQL: (sqlColumns: string[] | null) => set({ sqlColumns }),
+  setSQL: (sqlColumns: Object[] | null) => set({ sqlColumns }),
   setColumns: (columns: string[] | null) => set({ columns }),
 }));
