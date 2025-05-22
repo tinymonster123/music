4f68859607430aba53aac48d33847c6d44738c25
tinymonster123 <lfhxgs123@gmail.com>
2025-05-22 11:22:34 +0800

    fix: 优化 GET 请求处理逻辑，增加缓存检查和304响应支持

--- src/app/api/pieData/route.ts
+++ src/app/api/pieData/route.ts
@@ -6,20 +6,42 @@ export interface DatabaseConnection {
   conn: any;
 }
 
-const GET = async () => {
+const generateLastModified = () => {
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
+  return now.toUTCString();
+};
+
+const GET = async (request: Request) => {
+  // 检查客户端缓存
+  const ifModifiedSince = request.headers.get("if-modified-since");
+  const lastModified = generateLastModified();
+
+  if (ifModifiedSince && new Date(ifModifiedSince) >= new Date(lastModified)) {
+    // 数据未修改，返回 304 状态码
+    return new NextResponse(null, {
+      status: 304,
+      headers: {
+        "Last-Modified": lastModified,
+        "Cache-Control":
+          "max-age=3600, s-maxage=3600, stale-while-revalidate=86400",
+      },
+    });
+  }
   try {
     const { connection, conn } = (await connectDBSSH()) as DatabaseConnection;
 
     const [rows] = await connection.query(`
             SELECT album_listens, album_title  
             FROM raw_albums  
-            ORDER BY album_listens;
+            ORDER BY album_listens
+            LIMIT 1000;
         `);
 
     // console.log(rows);
 
-    await connection.end();
-    conn.end();
+    // await connection.end();
+    // conn.end();
 
     return NextResponse.json(
       {
@@ -29,6 +51,7 @@ const GET = async () => {
       {
         status: 200,
         headers: {
+          "Last-Modified": lastModified,
           "Cache-Control":
             "max-age=3600, s-maxage=3600, stale-while-revalidate=86400",
         },
